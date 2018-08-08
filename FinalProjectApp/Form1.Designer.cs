054a4c751e131967f9c0e568fb68def59945be66
pedro.campos <campos.pedrosouza@gmail.com>
2018-08-08 20:57:13 -0300

    primeiro commit

---
+++ FinalProjectApp/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace FinalProjectApp
+{
+    partial class frmFinalProjectApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dthInicio = new System.Windows.Forms.DateTimePicker();
+            this.dthFim = new System.Windows.Forms.DateTimePicker();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.btnUpdateData = new System.Windows.Forms.Button();
+            this.btnDeleteData = new System.Windows.Forms.Button();
+            this.piPointTagSearchPage1 = new OSIsoft.AF.UI.PropertyPage.PIPointTagSearchPage();
+            this.lblDthInicio = new System.Windows.Forms.Label();
+            this.lblDthFim = new System.Windows.Forms.Label();
+            this.chkShowSnapshot = new System.Windows.Forms.CheckBox();
+            this.btnClearData = new System.Windows.Forms.Button();
+            this.btnImportExcel = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dthInicio
+            // 
+            this.dthInicio.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            this.dthInicio.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dthInicio.Location = new System.Drawing.Point(360, 47);
+            this.dthInicio.Name = "dthInicio";
+            this.dthInicio.Size = new System.Drawing.Size(200, 20);
+            this.dthInicio.TabIndex = 0;
+            // 
+            // dthFim
+            // 
+            this.dthFim.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            this.dthFim.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dthFim.Location = new System.Drawing.Point(648, 47);
+            this.dthFim.Name = "dthFim";
+            this.dthFim.Size = new System.Drawing.Size(200, 20);
+            this.dthFim.TabIndex = 1;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(322, 122);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(733, 374);
+            this.dataGrid.TabIndex = 2;
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Location = new System.Drawing.Point(863, 47);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(114, 23);
+            this.btnGetData.TabIndex = 3;
+            this.btnGetData.Text = "Get Data";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // btnUpdateData
+            // 
+            this.btnUpdateData.Location = new System.Drawing.Point(560, 93);
+            this.btnUpdateData.Name = "btnUpdateData";
+            this.btnUpdateData.Size = new System.Drawing.Size(114, 23);
+            this.btnUpdateData.TabIndex = 4;
+            this.btnUpdateData.Text = "Update Data";
+            this.btnUpdateData.UseVisualStyleBackColor = true;
+            this.btnUpdateData.Click += new System.EventHandler(this.btnUpdateData_Click);
+            // 
+            // btnDeleteData
+            // 
+            this.btnDeleteData.Location = new System.Drawing.Point(680, 93);
+            this.btnDeleteData.Name = "btnDeleteData";
+            this.btnDeleteData.Size = new System.Drawing.Size(114, 23);
+            this.btnDeleteData.TabIndex = 5;
+            this.btnDeleteData.Text = "Delete Data";
+            this.btnDeleteData.UseVisualStyleBackColor = true;
+            this.btnDeleteData.Click += new System.EventHandler(this.btnDeleteData_Click);
+            // 
+            // piPointTagSearchPage1
+            // 
+            this.piPointTagSearchPage1.AccessibleDescription = "PI point tag search property page";
+            this.piPointTagSearchPage1.AccessibleName = "PI point tag search page";
+            this.piPointTagSearchPage1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.piPointTagSearchPage1.BackColor = System.Drawing.Color.Transparent;
+            this.piPointTagSearchPage1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.piPointTagSearchPage1.HelpContext = ((long)(0));
+            this.piPointTagSearchPage1.Location = new System.Drawing.Point(18, 35);
+            this.piPointTagSearchPage1.Margin = new System.Windows.Forms.Padding(0);
+            this.piPointTagSearchPage1.MinimumSize = new System.Drawing.Size(250, 300);
+            this.piPointTagSearchPage1.Name = "piPointTagSearchPage1";
+            this.piPointTagSearchPage1.Size = new System.Drawing.Size(292, 461);
+            this.piPointTagSearchPage1.TabIndex = 6;
+            // 
+            // lblDthInicio
+            // 
+            this.lblDthInicio.AutoSize = true;
+            this.lblDthInicio.Location = new System.Drawing.Point(319, 51);
+            this.lblDthInicio.Name = "lblDthInicio";
+            this.lblDthInicio.Size = new System.Drawing.Size(32, 13);
+            this.lblDthInicio.TabIndex = 7;
+            this.lblDthInicio.Text = "Start:";
+            // 
+            // lblDthFim
+            // 
+            this.lblDthFim.AutoSize = true;
+            this.lblDthFim.Location = new System.Drawing.Point(616, 51);
+            this.lblDthFim.Name = "lblDthFim";
+            this.lblDthFim.Size = new System.Drawing.Size(29, 13);
+            this.lblDthFim.TabIndex = 8;
+            this.lblDthFim.Text = "End:";
+            // 
+            // chkShowSnapshot
+            // 
+            this.chkShowSnapshot.AutoSize = true;
+            this.chkShowSnapshot.Location = new System.Drawing.Point(810, 97);
+            this.chkShowSnapshot.Name = "chkShowSnapshot";
+            this.chkShowSnapshot.Size = new System.Drawing.Size(101, 17);
+            this.chkShowSnapshot.TabIndex = 9;
+            this.chkShowSnapshot.Text = "Show Snapshot";
+            this.chkShowSnapshot.UseVisualStyleBackColor = true;
+            this.chkShowSnapshot.CheckedChanged += new System.EventHandler(this.chkShowSnapshot_CheckedChanged);
+            // 
+            // btnClearData
+            // 
+            this.btnClearData.Location = new System.Drawing.Point(322, 93);
+            this.btnClearData.Name = "btnClearData";
+            this.btnClearData.Size = new System.Drawing.Size(114, 23);
+            this.btnClearData.TabIndex = 10;
+            this.btnClearData.Text = "Clear Data";
+            this.btnClearData.UseVisualStyleBackColor = true;
+            this.btnClearData.Click += new System.EventHandler(this.btnClearData_Click);
+            // 
+            // btnImportExcel
+            // 
+            this.btnImportExcel.Location = new System.Drawing.Point(442, 93);
+            this.btnImportExcel.Name = "btnImportExcel";
+            this.btnImportExcel.Size = new System.Drawing.Size(114, 23);
+            this.btnImportExcel.TabIndex = 11;
+            this.btnImportExcel.Text = "Import Excel";
+            this.btnImportExcel.UseVisualStyleBackColor = true;
+            this.btnImportExcel.Click += new System.EventHandler(this.btnImportExcel_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmFinalProjectApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 511);
+            this.Controls.Add(this.btnImportExcel);
+            this.Controls.Add(this.btnClearData);
+            this.Controls.Add(this.chkShowSnapshot);
+            this.Controls.Add(this.lblDthFim);
+            this.Controls.Add(this.lblDthInicio);
+            this.Controls.Add(this.piPointTagSearchPage1);
+            this.Controls.Add(this.btnDeleteData);
+            this.Controls.Add(this.btnUpdateData);
+            this.Controls.Add(this.btnGetData);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.dthFim);
+            this.Controls.Add(this.dthInicio);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "frmFinalProjectApp";
+            this.Text = "App";
+            this.Load += new System.EventHandler(this.frmFinalProjectApp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dthInicio;
+        private System.Windows.Forms.DateTimePicker dthFim;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.Button btnUpdateData;
+        private System.Windows.Forms.Button btnDeleteData;
+        private OSIsoft.AF.UI.PropertyPage.PIPointTagSearchPage piPointTagSearchPage1;
+        private System.Windows.Forms.Label lblDthInicio;
+        private System.Windows.Forms.Label lblDthFim;
+        private System.Windows.Forms.CheckBox chkShowSnapshot;
+        private System.Windows.Forms.Button btnClearData;
+        private System.Windows.Forms.Button btnImportExcel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ FinalProjectApp/Form1.cs
@@ -0,0 +1,345 @@
+﻿using OSIsoft.AF;
+using OSIsoft.AF.Asset;
+using OSIsoft.AF.Data;
+using OSIsoft.AF.PI;
+using OSIsoft.AF.Time;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProjectApp
+{
+    delegate void SetDataGridCallback(DataTable dataTable);
+
+    public partial class frmFinalProjectApp : Form
+    {
+        private DataTable _dt = new DataTable();
+
+        private Thread _getSnapshotThread;
+
+        private PIPagingConfiguration _piPagingConfiguration = new PIPagingConfiguration(PIPageType.EventCount,100000);
+
+        public frmFinalProjectApp()
+        {
+            var culture = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+            InitializeComponent();
+            
+            _dt.Columns.Add("Tag", typeof(string));
+            _dt.Columns.Add("Timestamp", typeof(DateTime));
+            _dt.Columns.Add("Value", typeof(object));
+            _dt.Columns.Add("UOM", typeof(string));
+            _dt.Columns.Add("Annotation", typeof(string));
+            _dt.Columns.Add("CheckBox", typeof(bool));
+            _dt.Columns.Add("Message", typeof(string));
+
+            dataGrid.DataSource = _dt;
+            
+            dthFim.Value = DateTime.Now;
+            dthInicio.Value = dthFim.Value.AddHours(-1);
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            _dt.Rows.Clear();
+            
+            PIPointList piPointList = new PIPointList();
+            piPointList.AddRange(piPointTagSearchPage1.PIPoints);
+            
+            AFValues afValues = new AFValues();
+            
+            foreach (var piPoint in piPointList)
+            {
+                afValues.AddRange(piPoint.RecordedValues(new AFTimeRange(new AFTime(DateTime.SpecifyKind(dthInicio.Value, DateTimeKind.Local)), new AFTime(DateTime.SpecifyKind(dthFim.Value, DateTimeKind.Local))), AFBoundaryType.Inside, string.Empty, false));
+            }
+
+            foreach (var afValue in afValues)
+            {
+                _dt.Rows.Add(afValue.PIPoint.Name, (DateTime)afValue.Timestamp.LocalTime, afValue.Value, afValue.PIPoint.GetAttribute(PICommonPointAttributes.EngineeringUnits), afValue.GetAnnotation(), false, string.Empty);
+            }
+            
+            dataGrid.DataSource = _dt;
+        }
+
+        private void btnUpdateData_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGrid.Rows)
+            {
+                try
+                {
+                    if (row.Cells["CheckBox"].Value != null &&  (bool)row.Cells["CheckBox"].Value == true)
+                    {
+                        // Connect PIServerAFSDK
+                        PIServer _PIServerAFSDK = piPointTagSearchPage1.PIServer;
+
+                        // Find PIPoint
+                        PIPoint piPoint = PIPoint.FindPIPoint(_PIServerAFSDK, (string)row.Cells["tag"].Value);
+
+                        AFAttribute afAttribute = new AFAttribute(piPoint);
+                        
+                        AFValue afValue = new AFValue(afAttribute, row.Cells["Value"].Value, AFTime.Parse(row.Cells["Timestamp"].Value.ToString()));
+                        
+                        if(row.Cells["Annotation"].Value.ToString() != string.Empty)
+                        {
+                            afValue.SetAnnotation(row.Cells["Annotation"].Value.ToString());
+                        }
+                        
+                        piPoint.UpdateValue(afValue, AFUpdateOption.Replace, AFBufferOption.BufferIfPossible);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    row.Cells["Message"].Value = ex.Message;
+                }
+
+            }
+
+            dataGrid.Refresh();
+        }
+
+        private void btnDeleteData_Click(object sender, EventArgs e)
+        {
+
+            List<int> indexToRemove = new List<int>();
+
+            foreach (DataGridViewRow row in dataGrid.Rows)
+            {
+                if (row.Cells["CheckBox"].Value != null && (bool)row.Cells["CheckBox"].Value == true)
+                {
+                    try
+                    {
+                        // Connect PIServerAFSDK
+                        PIServer _PIServerAFSDK = piPointTagSearchPage1.PIServer;
+
+                        // Find PIPoint
+                        PIPoint piPoint = PIPoint.FindPIPoint(_PIServerAFSDK, (string)row.Cells["tag"].Value);
+
+                        AFAttribute afAttribute = new AFAttribute(piPoint);
+                        
+                        AFValue afValue = new AFValue(afAttribute, row.Cells["Value"].Value, AFTime.Parse(row.Cells["Timestamp"].Value.ToString()));
+                        
+                        piPoint.UpdateValue(afValue, AFUpdateOption.Remove, AFBufferOption.BufferIfPossible);
+
+                        if (!row.IsNewRow)
+                        {
+                            indexToRemove.Add(row.Index);
+                        }
+
+                    }
+                    catch(Exception ex)
+                    {
+                        row.Cells["Message"].Value = ex.Message;
+                    }
+                }
+
+            }
+
+            foreach (var index in indexToRemove.OrderByDescending(i => i))
+            {
+                dataGrid.Rows.RemoveAt(index);
+            }
+
+            dataGrid.Refresh();
+        }
+
+        private void chkShowSnapshot_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkShowSnapshot.Checked == true)
+            {
+                btnClearData.Enabled = false;
+                btnImportExcel.Enabled = false;
+                btnGetData.Enabled = false;
+                btnUpdateData.Enabled = false;
+                btnDeleteData.Enabled = false;
+                piPointTagSearchPage1.Enabled = false;
+
+                _getSnapshotThread = new Thread(getSnapshot);
+
+                _getSnapshotThread.Start(piPointTagSearchPage1.PIPoints);
+            }
+            else
+            {
+                btnClearData.Enabled = true;
+                btnImportExcel.Enabled = true;
+                btnGetData.Enabled = true;
+                btnUpdateData.Enabled = true;
+                btnDeleteData.Enabled = true;
+                piPointTagSearchPage1.Enabled = true;
+                _getSnapshotThread.Abort();
+            }            
+        }
+
+        private void btnClearData_Click(object sender, EventArgs e)
+        {
+            _dt.Rows.Clear();
+        }
+
+        private void btnImportExcel_Click(object sender, EventArgs e)
+        {
+            _dt.Rows.Clear();
+
+            string fname = "";
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "Excel File Dialog";
+            //fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                fname = fdlg.FileName;
+            }
+            
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(fname);
+            Microsoft.Office.Interop.Excel.Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Microsoft.Office.Interop.Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+            
+            for (int i = 2; i <= rowCount; i++)
+            {
+                try
+                {
+                    _dt.Rows.Add(Convert.ToString(xlRange.Cells[i, 1].Value2),
+                                 DateTime.FromOADate(double.Parse(xlRange.Cells[i, 2].Value2.ToString())).ToString("dd-MM-yyyy hh:mm:ss"),
+                                 Convert.ToString(xlRange.Cells[i, 3].Value2),
+                                 Convert.ToString(xlRange.Cells[i, 4].Value2),
+                                 Convert.ToString(xlRange.Cells[i, 5].Value2),
+                                 true,
+                                 string.Empty);
+                }
+                catch(Exception ex)
+                {
+                    _dt.Rows.Add(Convert.ToString(xlRange.Cells[i, 1].Value2),
+                                 DateTime.Now,
+                                 Convert.ToString(xlRange.Cells[i, 3].Value2),
+                                 Convert.ToString(xlRange.Cells[i, 4].Value2),
+                                 Convert.ToString(xlRange.Cells[i, 5].Value2),
+                                 true,
+                                 ex.Message);
+                }
+            }
+
+
+            dataGrid.DataSource = _dt;
+            
+            // Cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            
+            // Release com objects to fully kill excel process from running in the background  
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            // Close and release  
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            // Quit and release  
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+        }
+
+        private void getSnapshot(object obj)
+        {
+            List<PIPoint> p = obj as List<PIPoint>;
+
+            SetDataGridCallback _dataGridCallback = new SetDataGridCallback(SetDataTable);
+
+            PIDataPipe pipe = new PIDataPipe(AFDataPipeType.Archive);
+            pipe.AddSignups(p);
+
+            //_dt.Rows.Clear();
+            _dt = new DataTable();
+            
+            _dt.Columns.Add("Tag", typeof(string));
+            _dt.Columns.Add("Timestamp", typeof(DateTime));
+            _dt.Columns.Add("Value", typeof(object));
+            _dt.Columns.Add("UOM", typeof(string));
+            _dt.Columns.Add("Annotation", typeof(string));
+            _dt.Columns.Add("CheckBox", typeof(bool));
+            _dt.Columns.Add("Message", typeof(string));
+
+            PIPointList piPointList = new PIPointList();
+            piPointList.AddRange(p);
+            
+            AFValues afValues = new AFValues();
+
+            foreach (var piPoint in piPointList)
+            {
+                afValues.Add(piPoint.CurrentValue());
+            }
+
+
+            foreach (var afValue in afValues)
+            {
+                _dt.Rows.Add(afValue.PIPoint.Name, (DateTime)afValue.Timestamp.LocalTime, afValue.Value, afValue.PIPoint.GetAttribute(PICommonPointAttributes.EngineeringUnits), afValue.GetAnnotation(), false, string.Empty);
+            }
+            
+
+            this.Invoke(_dataGridCallback, _dt );
+            
+
+            while (chkShowSnapshot.Checked == true)
+            {
+                AFListResults<PIPoint, AFDataPipeEvent> pipeConstants = pipe.GetUpdateEvents(5000);
+                
+                foreach (AFDataPipeEvent pipeEvent in pipeConstants)
+                {
+                    foreach (DataRow row in _dt.Rows)
+                    {
+                        if (row["Tag"] == pipeEvent.Value.PIPoint.Name)
+                        {
+                            row["Timestamp"] = pipeEvent.Value.Timestamp.LocalTime;
+                            row["Value"] = pipeEvent.Value.Value;
+                            row["UOM"] = pipeEvent.Value.PIPoint.GetAttribute(PICommonPointAttributes.EngineeringUnits);
+                            row["Annotation"] = pipeEvent.Value.GetAnnotation();
+                        }
+                    }
+                }
+                
+                if (this.dataGrid.InvokeRequired)
+                {
+                    this.Invoke(_dataGridCallback, _dt);
+                }
+                else
+                {
+                    dataGrid.DataSource = _dt;
+                    dataGrid.Refresh();
+                }
+                
+            };
+            pipe.Close();
+            pipe.Dispose();
+        }
+        
+        private void SetDataTable(DataTable dataTable)
+        {
+            dataGrid.DataSource = dataTable;
+
+            dataGrid.Refresh();
+            Thread.Sleep(1000);
+        }
+
+        private void frmFinalProjectApp_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+    }
+}
